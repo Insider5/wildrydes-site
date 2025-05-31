d4f39cd8f1e92a420da5b48f685eb157a0955c2c
PENIKELAPATI TEJ PAVAN <123471811+Insider5@users.noreply.github.com>
2025-06-01 00:10:48 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-north-1_XbGEk0Sr1', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4iqtmamdqbtn0bd64f2opfata', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'eu-north-1_Q5RBN5zhn', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1nb7khj6p2nvnreao4n1qnmuh5', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
